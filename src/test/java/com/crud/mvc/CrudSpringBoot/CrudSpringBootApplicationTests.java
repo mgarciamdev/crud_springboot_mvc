c488ade3b028d16ee0a398fd3a560a96e41d9f25
mgarciamdev <mgarciadev117@gmail.com>
2024-02-14 19:37:06 -0600

    Creacion del proyecto

---
+++ src/test/java/com/crud/mvc/CrudSpringBoot/CrudSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.mvc.CrudSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
